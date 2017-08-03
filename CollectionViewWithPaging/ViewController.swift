2f25af640d6744b44b1342a77ae6358c1610d58d
Hershalle <hershalle@gmail.com>
2017-08-03 11:07:02 +0300

    initial commit

---
+++ CollectionViewWithPaging/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  CollectionViewWithPaging
+//
+//  Created by Shai Balassiano on 28/07/2017.
+//  Copyright © 2017 Shai Balassiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet private weak var collectionViewLayout: UICollectionViewFlowLayout!
+    
+    private var dataSource: [[UIColor]] = [[.bittersweet(), .blizzardBlue(), .blue(), .blueBell(), .blueGreen(), .blueViolet(), .blush(), .brickRed()],
+                                           [.almond(), .antiqueBrass(), .apricot(), .aquamarine(), .asparagus(), .atomicTangerine(), .bananaMania(), .beaver()],
+                                           [.brilliantRose(), .brown(), .burntOrange(), .burntSienna(), .cadetBlue(), .canary(), .caribbeanGreen(), .carnationPink()],
+                                           [.cerise(), .cerulean(), .chestnut(), .copperCrayolaAlternateColor(), .copper(), .cornflowerBlue(), .cottonCandy(), .dandelion()],
+                                           [.denim(), .desertSand(), .eggplant(), .electricLime(), .fern(), .forestGreen(), .fuchsia(), .fuzzyWuzzy()],
+                                           [.gold(), .goldenrod(), .grannySmithApple(), .gray(), .green(), .greenBlue(), .greenYellow(), .hotMagenta()],
+                                           [.inchworm(), .indigo(), .jazzberryJam(), .jungleGreen(), .laserLemon(), .lavender(), .lemonYellow(), .lightBlue()]]
+    
+    private var indexOfCellBeforeDragging = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionViewLayout.minimumLineSpacing = 0
+        configureCollectionViewLayoutItemSize()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        configureCollectionViewLayoutItemSize()
+    }
+    
+    private func configureCollectionViewLayoutItemSize() {
+        let inset: CGFloat = 25
+        collectionViewLayout.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+        
+        collectionViewLayout.itemSize = CGSize(width: collectionViewLayout.collectionView!.frame.size.width - inset * 2, height: collectionViewLayout.collectionView!.frame.size.height)
+        collectionViewLayout.collectionView!.reloadData()
+    }
+    
+    // ===================================
+    // MARK: - UICollectionViewDataSource:
+    // ===================================
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorsPaletteCollectionViewCell", for: indexPath) as! ColorsPaletteCollectionViewCell
+        
+        cell.configure(colors: dataSource[indexPath.row]) { (selectedColor: UIColor) in
+            self.view.backgroundColor = selectedColor
+        }
+        
+        // You can color the cells so you could see how they behave:
+        //        let isEvenCell = CGFloat(indexPath.row).truncatingRemainder(dividingBy: 2) == 0
+        //        cell.backgroundColor = isEvenCell ? UIColor(white: 0.9, alpha: 1) : .white
+        
+        return cell
+    }
+}
